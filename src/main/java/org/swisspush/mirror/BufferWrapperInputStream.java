e89fb4a0c13fcc833d1796c5c5f4736cffbaabb2
Oliver Henning <oliver.henning@post.ch>
2018-12-04 16:14:39 +0100

    Wrap Vertx Buffer as InputStream.
    We now can feed ZipInputStream from Vertx Buffers. No need to duplicate Vertx Buffer as byte[] in-memory

---
+++ src/main/java/org/swisspush/mirror/BufferWrapperInputStream.java
@@ -0,0 +1,49 @@
+package org.swisspush.mirror;
+
+import io.vertx.core.buffer.Buffer;
+
+import java.io.InputStream;
+
+/**
+ * Wraps a Vert.x Buffer to a classic Java-IO InputStream.
+ * We need this to handle Buffer content as a ZipInputStream
+ *
+ * Note that this avoids in-memory data duplication as happened when we used <pre>new ByteArrayInputStream(buffer.getBytes())</pre>
+ */
+public class BufferWrapperInputStream extends InputStream {
+
+    private final Buffer buffer;
+    private int pos = 0;
+
+    public BufferWrapperInputStream(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public int read() {
+        if (pos >= buffer.length()) {
+            return -1;
+        }
+        return buffer.getByte(pos++) & 0xFF;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) {
+        // few lines copied from java.io.InputStream to ensure same behaviour
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || len < 0 || len > b.length - off) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return 0;
+        }
+
+        int count = Integer.min(len, buffer.length() - pos);
+        if (count <= 0) {
+            return -1;
+        }
+        buffer.getBytes(pos, pos + count, b, off);
+        pos += count;
+        return count;
+    }
+}
